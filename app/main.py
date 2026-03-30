81a21e0aac63a3fdc42a15e7fe6d48b8f2b86c1a
Elliot Virtanen <elliotvir@gmail.com>
2026-03-30 11:09:54 +0300

    changes

--- app/main.py
+++ app/main.py
@@ -4,14 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
 
- app.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
